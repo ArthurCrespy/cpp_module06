78fd8b7522023e4d66ef426c4ef62bb53579ba2f
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-04-22 11:52:14 +0200

    Use of & instead of * and implement try/catch method

--- files/ex02/Base.cpp
+++ files/ex02/Base.cpp
@@ -48,12 +48,24 @@ void identify(Base *p)
 
 void identify(Base &p)
 {
-		if (dynamic_cast <A *>(&p))
-			std::cout << "A" << std::endl;
-		else if (dynamic_cast <B *>(&p))
-			std::cout << "B" << std::endl;
-		else if (dynamic_cast <C *>(&p))
-			std::cout << "C" << std::endl;
-		else
-			std::cout << "Unknown type" << std::endl;
+	try
+	{
+		(void)dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::exception &e) {}
+
+	try
+	{
+		(void)dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch(const std::exception &e) {}
+
+	try
+	{
+		(void)dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch(const std::exception &e) {}
 }
